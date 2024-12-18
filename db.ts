29bc1df092e14652d90baa988918b1d15ed48317
afolabi <yusufafolabi95@gmail.com>
2024-12-18 21:36:16 +0100

    waitlist

--- db.ts
+++ db.ts
@@ -1,16 +1,12 @@
-import mongoose, { ConnectOptions } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 const DB_URI = process.env.MONGODB_URI || "";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as ConnectOptions);
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(DB_URI);
+    console.log(`MongoDB Connected`);
   } catch (err) {
     console.error(err);
     process.exit(1);
