5db9f2c70aa09825f6e50848d6b32be9f5ec5356
afolabi <yusufafolabi95@gmail.com>
2025-04-11 15:26:13 +0100

    Update cron job schedule to run daily at 2AM for fund movement

--- src/cronJob/cron.ts
+++ src/cronJob/cron.ts
@@ -2,6 +2,6 @@ import cron from "node-cron";
 import { moveFundsAfterFiveDays } from "./job";
 
 
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 2 * * *", async () => {
   await moveFundsAfterFiveDays(); // runs every day at 2AM
 });
