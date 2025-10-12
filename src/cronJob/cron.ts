8188a0ce751392daa049be52db10091e5f34535b
Afolabi <81269981+Dev-folabi@users.noreply.github.com>
2025-10-12 10:07:49 +0100

    updated

--- src/cronJob/cron.ts
+++ src/cronJob/cron.ts
@@ -1,13 +1,12 @@
 import cron from "node-cron";
 import { moveFundsAfterFiveDays } from "./job";
 import { unreserveExpiredProducts } from "./unreserveProducts";
+import { cancelExpiredTransactions } from "./cancelExpiredTransactions";
 
 
 cron.schedule("0 2 * * *", async () => {
   await moveFundsAfterFiveDays(); // runs every day at 2AM
 });
-
-import { cancelExpiredTransactions } from "./cancelExpiredTransactions";
 
 cron.schedule("*/5 * * * *", async () => {
   await unreserveExpiredProducts(); // runs every 5 minutes
